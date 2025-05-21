9df36682f735a972fce974f30769cdc976459056
swaraydev <swaray.kmariame@gmail.com>
2025-05-21 16:12:58 +0200

    modified app.js index.html styles.css

--- 15ProjectJavaScript/app.js
+++ 15ProjectJavaScript/app.js
@@ -1,3 +1,16 @@
 /* select date */
 const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
+
+// const topLink = document.querySelector(".top-link");
+// const scrollLinks = document.querySelector(".scroll-link");
+
+// scrollLinks.forEach(function (link) {
+//   link.addEvenListener("click", function (e) {
+//     e.preventDefault();
+//     window.scrollTo({
+//       left: 0,
+//       top: position,
+//     });
+//   });
+// });
